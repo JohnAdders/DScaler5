83c8fd56b3bfbe2bfcfbd6c7dfcb56d4d640d053
adcockj <adcockj@users.sourceforge.net>
2004-03-09 20:21:56 +0000

    Make persistence work properly
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler5@3538 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

--- Filters/Processing/DScalerFilter/DScaler.h
+++ Filters/Processing/DScalerFilter/DScaler.h
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
-// $Id: DScaler.h,v 1.4 2004-03-05 17:21:33 adcockj Exp $
+// $Id: DScaler.h,v 1.5 2004-03-09 20:21:56 adcockj Exp $
 ///////////////////////////////////////////////////////////////////////////////
 // DScalerFilter.dll - DirectShow filter for deinterlacing and video processing
 // Copyright (c) 2003 John Adcock
@@ -42,6 +42,7 @@ IMPLEMENT_AGGREGATABLE_COCLASS(CDScaler, "{0D71870A-7563-11D7-B84A-0002A5623377}
     IMPLEMENTS_INTERFACE(IMediaParams)
     IMPLEMENTS_INTERFACE(IMediaParamInfo)
     IMPLEMENTS_INTERFACE(IPersistStream)
+    IMPLEMENTS_INTERFACE_AS(IPersist, IPersistStream)
 END_INTERFACE_TABLE()
 
 public:
