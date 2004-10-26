07004bebecdcda45d5adc3e726d20571f8a99992
adcockj <adcockj@users.sourceforge.net>
2004-10-26 16:35:03 +0000

    Whoops - should be GPL not LGPL
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler5@3759 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

--- Filters/Source/TSReader/TSReader.cpp
+++ Filters/Source/TSReader/TSReader.cpp
@@ -1,25 +1,29 @@
 ///////////////////////////////////////////////////////////////////////////////
-// $Id: TSReader.cpp,v 1.1 2004-10-26 16:26:54 adcockj Exp $
+// $Id: TSReader.cpp,v 1.2 2004-10-26 16:35:03 adcockj Exp $
 ///////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2003 John Adcock
 ///////////////////////////////////////////////////////////////////////////////
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
+//  This file is subject to the terms of the GNU General Public License as
+//  published by the Free Software Foundation.  A copy of this license is
+//  included with this software distribution in the file COPYING.  If you
+//  do not have a copy, you may obtain a copy by writing to the Free
+//  Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+//  This software is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details
 // 
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
-// 
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
+// You should have received a copy of the GNU General Public
+// License along with this package; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ///////////////////////////////////////////////////////////////////////////////
 // CVS Log
 //
 // $Log: not supported by cvs2svn $
+// Revision 1.1  2004/10/26 16:26:54  adcockj
+// Added new Transport stream source filter
+//
 ///////////////////////////////////////////////////////////////////////////////
 #include "stdafx.h"
 #include "TSReader.h"
@@ -107,7 +111,7 @@ STDMETHODIMP CTSReader::get_License(eFreeLicense* License)
     {
         return E_POINTER;
     }
-    *License = LGPL;
+    *License = GPL;
     return S_OK;
 }
 
--- Filters/Source/TSReader/TSReaderOutPin.h
+++ Filters/Source/TSReader/TSReaderOutPin.h
@@ -1,20 +1,21 @@
 ///////////////////////////////////////////////////////////////////////////////
-// $Id: TSReaderOutPin.h,v 1.1 2004-10-26 16:26:54 adcockj Exp $
+// $Id: TSReaderOutPin.h,v 1.2 2004-10-26 16:35:03 adcockj Exp $
 ///////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2003 John Adcock
 ///////////////////////////////////////////////////////////////////////////////
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
+//  This file is subject to the terms of the GNU General Public License as
+//  published by the Free Software Foundation.  A copy of this license is
+//  included with this software distribution in the file COPYING.  If you
+//  do not have a copy, you may obtain a copy by writing to the Free
+//  Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+//  This software is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details
 // 
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
-// 
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
+// You should have received a copy of the GNU General Public
+// License along with this package; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ///////////////////////////////////////////////////////////////////////////////
 
